24980da970850f1074ccdd2860feb71a3d8f54a5
Dendi <lucky@DendideMacBook-Pro.local>
2018-05-05 14:23:54 +0800

    judge mongo use

--- src/main/java/com/lucky/douyu/models/MongoHelper.java
+++ src/main/java/com/lucky/douyu/models/MongoHelper.java
@@ -18,9 +18,9 @@ public class MongoHelper {
     private static MongoHelper sInstance;
 
     private MongoCollection<Document> collection;
-    private boolean useMongo = false;
 
-    private MongoHelper(){}
+    private MongoHelper() {
+    }
 
     public static MongoHelper getInstance() {
         if (sInstance == null) {
@@ -30,25 +30,20 @@ public class MongoHelper {
         return sInstance;
     }
 
-    public void init(String roomId) {
-        if ("1209".equals(roomId)) {
-            useMongo = true;
-            MongoClient client = MongoClients.create(Constants.Mongo.CONNECT_STRING);
-            MongoDatabase database = client.getDatabase(Constants.Mongo.DATABASE);
-            initCollection(database);
-            System.out.println("mongo client init");
-        }
+    public void init() {
+        MongoClient client = MongoClients.create(Constants.Mongo.CONNECT_STRING);
+        MongoDatabase database = client.getDatabase(Constants.Mongo.DATABASE);
+        initCollection(database);
+        System.out.println("mongo client init");
     }
 
     public void insertDocs(List<Document> docs) {
-        if (useMongo) {
-            collection.insertMany(docs);
-            System.out.println("insert documents");
-        }
+        collection.insertMany(docs);
+        System.out.println("insert documents");
     }
 
     private void initCollection(MongoDatabase database) {
-        for(String name : database.listCollectionNames()) {
+        for (String name : database.listCollectionNames()) {
             if (name.equals(Constants.Mongo.COLLECTION)) {
                 collection = database.getCollection(Constants.Mongo.COLLECTION);
                 return;
---
+++ src/main/java/com/lucky/douyu/utils/MD5Util.java
@@ -0,0 +1,25 @@
+package com.lucky.douyu.utils;
+
+
+import java.security.MessageDigest;
+
+class MD5Util {
+    public static String MD5(String s) {
+        char hexDigits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+        try {
+            MessageDigest mdInst = MessageDigest.getInstance("MD5");
+            byte[] md = mdInst.digest(s.getBytes());
+            int j = md.length;
+            char str[] = new char[j * 2];
+            int k = 0;
+            for (byte b : md) {
+                str[k++] = hexDigits[b >>> 4 & 0xf];
+                str[k++] = hexDigits[b & 0xf];
+            }
+            return new String(str).toLowerCase();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
