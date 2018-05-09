d3a12c1be5d63a4021728bdaf0af4e801cf533ce
Dendi <lucky@DendideMacBook-Pro.local>
2018-05-09 12:10:56 +0800

    save

--- src/main/java/com/lucky/douyu/Constants.java
+++ src/main/java/com/lucky/douyu/Constants.java
@@ -4,14 +4,14 @@ public interface Constants {
 
     interface Mongo {
         Boolean USE_MONGO = false;        // 是否使用mongo
-        //String CONNECT_STRING = "mongodb://localhost:27017";
-        String CONNECT_STRING = "mongodb://39.107.252.8:27017";
+        String CONNECT_STRING = "mongodb://localhost:27017";
+        //String CONNECT_STRING = "mongodb://39.107.252.8:27017";
         String DATABASE = "bullet_screen";  // mongo数据库名称
         String COLLECTION = "nd_1209";     // 集合名称
     }
 
     interface BulletScreenReceive {
-        String ROOM_ID ="1559610";      // 接收某房间的弹幕
+        String ROOM_ID ="1209";      // 接收某房间的弹幕
         String GROUP_ID = "-9999";   // 弹幕分组，海量弹幕模式
     }
 
