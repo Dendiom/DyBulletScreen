f8f91980f6243197607f0b32207272744a205405
Dendi <lucky@DendideMacBook-Pro.local>
2018-05-09 12:24:52 +0800

    save

--- src/main/java/com/lucky/douyu/Constants.java
+++ src/main/java/com/lucky/douyu/Constants.java
@@ -3,16 +3,16 @@ package com.lucky.douyu;
 public interface Constants {
 
     interface Mongo {
-        Boolean USE_MONGO = false;        // 是否使用mongo
+        Boolean USE_MONGO = true;        // 是否使用mongo
         String CONNECT_STRING = "mongodb://localhost:27017";
         String DATABASE = "bullet_screen";  // mongo数据库名称
-        String COLLECTION = "collection_name";     // 集合名称
+        String COLLECTION = "nd_1209";     // 集合名称
     }
 
     interface BulletScreenReceive {
         String ROOM_ID = "1209";      // 接收某房间的弹幕
         String GROUP_ID = "-9999";   // 弹幕分组，海量弹幕模式
-        int LEVEL_LIMIT = 5;         // 忽略几级以下的弹幕
+        int LEVEL_LIMIT = 8;         // 忽略几级以下的弹幕
     }
 
     // 以下均来自浏览器cookie
